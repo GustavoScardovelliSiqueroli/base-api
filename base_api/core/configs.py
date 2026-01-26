4a80775b28074008cf9a99908da350045bd07240
gustavo <gustavosecardovelli@gmail.com>
2026-01-26 13:49:31 -0300

    feat: add hash algorithm on settings

--- base_api/core/configs.py
+++ base_api/core/configs.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
     ACCESS_TOKEN_EXPIRE_HOURS: int = 24
+    JWT_ALGORITHM: str = 'HS256'
 
     API_KEY: str
 
